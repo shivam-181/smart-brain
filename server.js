99e1409aa323ef3d3e4d28692f4b3fa4f1bcafc1
shivam-181 <shivamkumar060703@gmail.com>
2025-03-22 01:05:16 +0530

    Fixed CORS issue

--- server.js
+++ server.js
@@ -23,9 +23,8 @@ const db = knex({
 
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors({
-    origin: 'https://smart-brain-fe.vercel.app',  // Replace with your actual frontend URL
+    origin: 'https://smart-brain-fe.vercel.app',  // Allow frontend URL
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
 }));
